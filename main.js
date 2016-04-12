e59ed2f3045830fd5773b4a649cbc28050de57d0
kangmail <tovikmail@gmail.com>
2016-04-13 01:02:58 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 <script type='text/javascript'>
 //<![CDATA[
 $(document).ready(function() {
-    // Menentukan elemen yang dijadikan sticky yaitu #header-wrapper
     var stickyNavTop = $('#nav').offset().top; 
     var stickyNav = function(){
         var scrollTop = $(window).scrollTop();  
-        // Kondisi jika discroll maka menu akan selalu diatas, dan sebaliknya        
         if (scrollTop > stickyNavTop) { 
             $('#nav').css({ 'position': 'fixed', 'top':0, 'z-index':9999 });
         } else {
             $('#nav').css({ 'position': 'relative' });
         }
     };
-    // Jalankan fungsi
     stickyNav();
     $(window).scroll(function() {
         stickyNav();
