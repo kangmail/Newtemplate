23efe80c38beac6320974737c9edf78daa1a4de2
kangmail <tovikmail@gmail.com>
2016-04-13 00:44:11 +0700

    Update main.js

--- main.js
+++ main.js
@@ -18,11 +18,5 @@ $(document).ready(function() {
         stickyNav();
     });
 });
- (function() {
-    var dr = document.createElement('script');
-    dr.type = 'text/javascript'; dr.async = true;
-    dr.src = '//share.donreach.com/buttons.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dr);
-  })();
 //]]>
 </script>
